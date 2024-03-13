0cb3172a6eaf4940d97580061e6029e50c496620
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-13 23:27:42 +0100

    fix css show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,54 +3,22 @@
 @extends('layouts.app')
 @section('main-content')
 <div id="show">
-        <div id="description">
-        <div id="left-description" >
+    <div class="container">
+        <div class="left">
+        <img src="{{$comic['thumb']}}" alt="">
+        </div>
+        <div class="right">
             <h1>{{$comic['title']}}</h1>
-            <div id="availability"> 
-                <div>         
-                    <span>U.S Price {{$comic['price']}}<span> 
-                </div>
-                <div>
-                    <span>AVAILABLE</span>
-                   <button>Check Availability</button>
-                </div>
-            </div>
-            <p>{{$comic['description']}}</p>
-        </div>
-        <div id="right-description">
-            <div id="image">
-                <img src="{{$comic['thumb']}}" alt="">
-            </div>
-        </div>
-    </div>
-    <div id="container-info">
-    <div id="info">
-        <div id="talent">
+            <p><strong>Descrizione:</strong>{{$comic['description']}}</p>
             <div>
-            <h3>Talent</h3>
-        </div>
-            <div>
-                <h5>Art by:</h5>
-           <p>{{$comic['artists']}}</p>
-        </div>
-        <div>
-            <h5>Written by:</h5>
-            <p>{{$comic['writers']}}</p>
-        </div>
-        <div id="spec">
-            <h3>Specs</h3>
-            <div><h5>Series:</h5>
-                <span>{{$comic['series']}}</span>
-            </div>
-            <div><h5>U.S.Price:</h5>
-                <span>{{$comic['price']}}</span>
-            </div>
-            <div><h5>On Sale Date:</h5>
-                <span>{{$comic['sale_date']}}</span>
+                <h4>Prezzo:{{$comic['price']}}</h4>
+                <h4>Data di rilascio:{{$comic['sale_date']}}</h4>
+                <h4>Autori:{{$comic['artists']}}</h4>
+                <h4>Scrittori:{{$comic['writers']}}</h4>
             </div>
         </div>
     </div>
 </div>
-</div>
+
 @endsection
     
